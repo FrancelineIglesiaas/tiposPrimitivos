e4970d96ff9c52daf9d361d31c0431f6944d00a2
Franceline Iglesias <145945337+FrancelineIglesiaas@users.noreply.github.com>
2023-10-01 13:49:24 -0300

    Update typeBoolean.js

---
+++ typeBoolean.js
@@ -0,0 +1,13 @@
+//Atribuindo variaveis//
+const primeiroNumero = 5;
+const segundoNumero = 5;
+let cadastroAtivado = true;
+
+//Comparações // true - false //
+console.log(primeiroNumero === segundoNumero); //true
+
+const texto1 = 'ALURA'
+const texto2 = 'alura';
+
+console.log(texto1 === texto2); //false
+//valores variaveis

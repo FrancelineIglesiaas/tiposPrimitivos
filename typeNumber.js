938ffc9ae426c52df1875c4baca6ac65dca876f9
Franceline Iglesias <145945337+FrancelineIglesiaas@users.noreply.github.com>
2023-10-01 13:33:47 -0300

    Add files via upload

---
+++ typeNumber.js
@@ -0,0 +1,38 @@
+// tipo number//
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+const operacaoMatematica = primeiroNumero + segundoNumero; 
+
+//ponto flutuante//
+
+console.log(operacaoMatematica);
+
+const numeroPontoFlutuante = 3.3;
+const pontoFlutuante= .5;
+
+const novaOperacao =  primeiroNumero / numeroPontoFlutuante;
+
+console.log(novaOperacao);
+
+// NAN // --> NOT A NUMBER//
+const alura = 'Alura';
+console.log(alura * primeiroNumero);
+
+//Divisão por zero não é um erro em JavaScript: ele simplesmente retorna “Infinity”//
+var a = 10
+var b = 0
+console.log(a/b) // Infinity
+
+//zero dividido por zero não tem um valor bem definido e o resultado dessa operação é o valor especial não numérico NaN.//
+var a = 0
+var b = 0
+console.log(a/b) // NaN
+
+
+
+
+
+
